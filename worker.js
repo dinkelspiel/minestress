fa821bb1f3f903f94864cda472882a151ce1be1d
dinkelspiel <mail@keii.dev>
2026-03-31 16:47:01 +0200

    feat: moving and block breaking

--- worker.js
+++ worker.js
@@ -1,8 +1,14 @@
 const { parentPort, workerData } = require("worker_threads");
 const mineflayer = require("mineflayer");
+const mcData = require("minecraft-data");
+const { pathfinder, Movements, goals } = require("mineflayer-pathfinder");
 
 const { config, startIndex, count } = workerData;
 const bots = [];
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function createBot(index) {
   const username = `${config.prefix}${index}`;
@@ -15,10 +21,19 @@ function createBot(index) {
     hideErrors: true,
   });
 
+  bot.loadPlugin(pathfinder);
   bot._client.on("error", () => {});
 
   bot.once("spawn", () => {
     parentPort.postMessage({ type: "spawned", username });
+
+    const data = mcData(bot.version);
+    const moves = new Movements(bot, data);
+    moves.allowSprinting = true;
+    bot.pathfinder.setMovements(moves);
+
+    scheduleWander(bot);
+    scheduleDig(bot, data);
   });
 
   bot.on("kicked", (reason) => {
@@ -32,6 +47,41 @@ function createBot(index) {
   });
 
   bots.push(bot);
+}
+
+function scheduleWander(bot) {
+  const wander = () => {
+    if (!bot.entity) return;
+    const pos = bot.entity.position;
+    const x = pos.x + randInt(-30, 30);
+    const z = pos.z + randInt(-30, 30);
+    try {
+      bot.pathfinder.setGoal(new goals.GoalNear(x, pos.y, z, 2), false);
+    } catch (_) {}
+    setTimeout(wander, randInt(5000, 15000));
+  };
+  setTimeout(wander, randInt(2000, 6000));
+}
+
+function scheduleDig(bot, data) {
+  const breakable = new Set(
+    Object.values(data.blocksByName)
+      .filter((b) => b.diggable && b.hardness != null && b.hardness > 0 && b.hardness <= 2.5)
+      .map((b) => b.id),
+  );
+
+  const dig = async () => {
+    if (!bot.entity) return;
+    try {
+      const target = bot.findBlock({
+        matching: (block) => breakable.has(block.type),
+        maxDistance: 4,
+      });
+      if (target) await bot.dig(target);
+    } catch (_) {}
+    setTimeout(dig, randInt(4000, 12000));
+  };
+  setTimeout(dig, randInt(5000, 15000));
 }
 
 async function run() {
